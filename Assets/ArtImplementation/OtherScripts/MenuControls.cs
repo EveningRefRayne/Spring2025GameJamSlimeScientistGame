f6ae651636736f8e821ad4043d59f3c41d2ee968
UzumeCoffee <uzumecoffee@proton.me>
2025-04-27 17:15:32 -0400

    Started linking everything, making playable

--- Assets/ArtImplementation/OtherScripts/MenuControls.cs
+++ Assets/ArtImplementation/OtherScripts/MenuControls.cs
@@ -3,8 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuControls : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private Button StartButton;
-    [SerializeField] private Button QuitButton;
+    public void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    /*
+    public void showAbout()
+    {
+        if (!showAboutText)
+        {
+            aboutText.SetActive(true);
+            showAboutText = true;
+        } else
+        {
+            aboutText.SetActive(false);
+            showAboutText = false;
+        }
+    }*/
 }
